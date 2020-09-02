e81bf2fde97e242df22fd122f8c19f7ce0829d24
senko88 <kostyanchik88@gmail.com>
2020-09-02 12:52:02 +0300

    start testing

--- src/main/java/ru/senkot/config/TestBeanConfig.java
+++ src/main/java/ru/senkot/config/TestBeanConfig.java
@@ -1,10 +1,16 @@
-//package ru.senkot.config;
-//
-//import org.springframework.context.annotation.ComponentScan;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//@ComponentScan(basePackages = "ru.senkot")
-//public class TestBeanConfig {
-//
-//}
+package ru.senkot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import ru.senkot.servicies.PatientService;
+
+@Configuration
+@ComponentScan(basePackages = "ru.senkot")
+public class TestBeanConfig {
+
+    @Bean
+    public PatientService getPatientService() {
+        return new PatientService();
+    }
+}
--- src/main/java/ru/senkot/messaging/MessageReceiver.java
+++ src/main/java/ru/senkot/messaging/MessageReceiver.java
@@ -11,11 +11,11 @@ import javax.jms.JMSException;
 @Component
 public class MessageReceiver {
 
-    private static final String ORDER_RESPONSE_QUEUE = "super-queue";
-
-    @JmsListener(destination = ORDER_RESPONSE_QUEUE)
-    public void receiveMessage(final Message<Event> message) throws JMSException {
-        MessageHeaders headers =  message.getHeaders();
-    }
+//    private static final String ORDER_RESPONSE_QUEUE = "super-queue";
+//
+//    @JmsListener(destination = ORDER_RESPONSE_QUEUE)
+//    public void receiveMessage(final Message<Event> message) throws JMSException {
+//        MessageHeaders headers =  message.getHeaders();
+//    }
 
 }
